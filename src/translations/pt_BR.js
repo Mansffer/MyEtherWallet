82bcd4096d5b4a7f9009dc4e1c2e47e138f5ef7b
Mansffer <mansfferpro@gmail.com>
2021-05-04 21:51:02 -0300

    update pt_BR.js

---
+++ src/translations/pt_BR.js
@@ -0,0 +1,95 @@
+/* eslint camelcase: 0 */
+import common from './common/pt_BR';
+import confirmation from './confirmation/pt_BR';
+import dappsMaker from './dapps-maker/pt_BR';
+import dappsSubmission from './dapps-submission/pt_BR';
+import home from './home/pt_BR';
+import footer from './footer/pt_BR';
+import gettingStarted from './getting-started/pt_BR';
+import createWallet from './create-wallet/pt_BR';
+import accessWallet from './access-wallet/pt_BR';
+import interfaceTranslations from './interface/pt_BR';
+import team from './team/pt_BR';
+import popover from './popover/pt_BR';
+import withoutWallet from './without-wallet/pt_BR';
+import errorsGlobal from './errors-global/pt_BR';
+import trezorError from './errors-trezor/pt_BR';
+import bcvaultError from './errors-bcvault/pt_BR';
+import coolWalletError from './errors-coolwallet/pt_BR';
+import ledgerError from './errors-ledger/pt_BR';
+import secalotError from './errors-secalot/pt_BR';
+import keepkeyError from './errors-keepkey/pt_BR';
+import bitboxError from './errors-bitbox/pt_BR';
+import bitbox02Error from './errors-bitbox02/pt_BR';
+import buyHardwareWallet from './other/buy-hardware-wallet/pt_BR';
+import convertUnits from './other/convert-units/pt_BR';
+import verifyMessage from './other/verify-message/pt_BR';
+import welcomeMessage from './welcome-message/pt_BR';
+import sendTx from './send-tx/pt_BR';
+import swap from './swap/pt_BR';
+import contract from './contract/pt_BR';
+import ens from './dapps-ens/pt_BR';
+import unstoppable from './dapps-unstoppable/pt_BR';
+import subDomain from './dapps-sub-domain/pt_BR';
+import scheduleTx from './dapps-schedule-tx/pt_BR';
+import nftManager from './nft-manager/pt_BR';
+import signMessage from './sign-message/pt_BR';
+import helpCenter from './help-center/pt_BR';
+import notifications from './notifications/pt_BR';
+import mewcx from './mew-cx/pt_BR';
+import dappsAmbrpay from './dapps-ambrpay/pt_BR';
+import phishing from './other/phishing/pt_BR';
+import dappsAave from './dapps-aave/pt_BR';
+import header from './header/pt_BR';
+import dappsMCDMaker from './dapps-mcd-maker/pt_BR';
+import dappsStaked from './dapps-staked/pt_BR';
+import dappsMisc from './dapps-misc/pt_BR';
+
+const pt_BR = {
+  common: common,
+  confirmation: confirmation,
+  home: home,
+  footer: footer,
+  gettingStarted: gettingStarted,
+  team: team,
+  accessWallet: accessWallet,
+  createWallet: createWallet,
+  interface: interfaceTranslations,
+  popover: popover,
+  withoutWallet: withoutWallet,
+  errorsGlobal: errorsGlobal,
+  trezorError: trezorError,
+  coolWalletError: coolWalletError,
+  ledgerError: ledgerError,
+  bcvaultError: bcvaultError,
+  secalotError: secalotError,
+  keepkeyError: keepkeyError,
+  bitboxError: bitboxError,
+  bitbox02Error: bitbox02Error,
+  buyHardwareWallet: buyHardwareWallet,
+  dappsMaker: dappsMaker,
+  convertUnits: convertUnits,
+  verifyMessage: verifyMessage,
+  welcomeMessage: welcomeMessage,
+  sendTx: sendTx,
+  swap: swap,
+  contract: contract,
+  ens: ens,
+  unstoppable: unstoppable,
+  subDomain: subDomain,
+  scheduleTx: scheduleTx,
+  nftManager: nftManager,
+  signMessage: signMessage,
+  dappsSubmission: dappsSubmission,
+  helpCenter: helpCenter,
+  notifications: notifications,
+  mewcx: mewcx,
+  dappsAmbrpay: dappsAmbrpay,
+  phishing: phishing,
+  dappsAave: dappsAave,
+  header: header,
+  dappsMCDMaker: dappsMCDMaker,
+  dappsStaked: dappsStaked,
+  dappsMisc: dappsMisc
+};
+export default pt_BR;
